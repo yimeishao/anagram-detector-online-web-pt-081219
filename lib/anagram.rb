4bd61178d1202637270e5ce5924d81e2b87e1e1f
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-09-16 19:16:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(words)
+
+    words.delete_if{|item| item.split("").sort != @word.split("").sort}
+
+  end
+end
