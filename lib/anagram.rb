8639b08eacf59ab5d87d3021a6dcfbd16b331c57
Yimei <maria.yimei.shao@gmail.com>
2019-09-16 17:30:58 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+attr_accessor :word
+
+def initialize(word)
+    @word = word 
+end 
+
+def match(array)
+    word_sorted = word.chars.sort
+    array.select {|x| x.chars.sort == word_sorted}
+end 
+
+end 
